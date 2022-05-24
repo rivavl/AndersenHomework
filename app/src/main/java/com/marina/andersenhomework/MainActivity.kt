5521c062ec8d582bf6fd99e445f3ce1f0842756e
rivavl <mvkalinenkova@yandex.ru>
2022-05-25 01:40:40 +0300

    homework 3

--- app/src/main/java/com/marina/andersenhomework/MainActivity.kt
+++ app/src/main/java/com/marina/andersenhomework/MainActivity.kt
@@ -1,11 +1,85 @@
 package com.marina.andersenhomework
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeView()
+
+        editText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                loadImage(editText.text.toString(), imageView)
+            }
+
+        })
+    }
+
+    private fun initializeView() {
+        imageView = findViewById(R.id.image_view)
+        editText = findViewById(R.id.edit_text)
+    }
+
+    private fun loadImage(url: String, imageView: ImageView) {
+        Glide.with(this)
+            .load(url)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    if (URL_REGEX.toRegex().matches(url)) {
+                        Toast.makeText(this@MainActivity, "Loading error", Toast.LENGTH_SHORT).show()
+                    }
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+            })
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(imageView)
+
+    }
+
+    companion object {
+        private const val URL_REGEX = "https?://picsum.photos[1234567890/]{2,}"
     }
 }
